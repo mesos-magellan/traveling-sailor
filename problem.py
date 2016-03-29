5ccecce40d7eba683487ae73af62b022d6f97e6e
Hamza Faran <hamzafaran@outlook.com>
2016-03-29 05:33:04 -0700

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# https://github.com/perrygeo/simanneal/blob/master/LICENSE.txt
-# https://github.com/perrygeo/simanneal/blob/master/examples/salesman.py
-
 from pyrallelsa.examples.tsp import TSPProblem
 
 Problem = TSPProblem
